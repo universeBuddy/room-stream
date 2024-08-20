1ed2547632fed7cad7fa0ce37f18fe67043b8386
universeBuddy <rai358788@gmail.com>
2024-08-21 04:38:59 +0530

    camera and microphone added

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import React, { useState } from "react";
 
+type CallLayoutRoom = "grid" | "speker-left" | "speker-right";
 const MeetingRoom = () => {
-  return (
-    <div>MeetingRoom</div>
-  )
+  const [layout, setLayout] = useState<CallLayoutType>("speker-left");
+
+  const [showParticipants, setShowParticipants] = useState(false)
+
+
+const CallLayout = ()=>{
+
+
+  switch(layout){
+    case 'grid':
+      return <PaginatedGridLayout />
+
+      case 'speker-right':
+        return <SpeakerLayout 
+        participantsBarPosition='left' />
+      default:
+        return <SpeakerLayout 
+        participantsBarPosition='right' />
+      
+  }
 }
 
-export default MeetingRoom
+
+
+  return (
+    <section className="relative h-screen w-full oveflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px] items-center ">
+          <CallLayout />
+        </div>
+        <div className="h-[calc(100vh-80px)] hidden ml-2"></div>
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
--- components/MeetingSetup.tsx
+++ components/MeetingSetup.tsx
@@ -1,18 +1,59 @@
 "use client";
 
-import { useCall, VideoPreview } from "@stream-io/video-react-sdk";
+import {
+  DeviceSettings,
+  useCall,
+  VideoPreview,
+} from "@stream-io/video-react-sdk";
 import React, { useEffect, useState } from "react";
 
-const MeetingSetup = () => {
+const MeetingSetup = ({
+  setIsSetupComplete,
+}: {
+  setIsSetupComplete: (value:boolean) => void;
+}) => {
   const [isMicCamToggledOn, setIsMicCamToggledOn] = useState(false);
 
   const call = useCall();
 
-  useEffect(() => {}, [isMicCamToggledOn, call?.camera, call.microphone]);
+  if (!call) {
+    throw new Error("usecall must be used within StreamCall Component");
+  }
+
+  useEffect(() => {
+    if (isMicCamToggledOn) {
+      call?.camera.disable();
+      call?.microphone.disable();
+    } else {
+      call?.camera.enable();
+      call?.microphone.enable();
+    }
+  }, [isMicCamToggledOn, call?.camera, call?.microphone]);
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-3 text-white">
       <h1 className="text-2xl font-bold">Setup</h1>
       <VideoPreview />
+
+      <div className="flex h-16 items-center justify-center gap-3">
+        <label className="flex items-center justify-center gap-2 font-medium">
+          <input
+            type="checkbox"
+            onChange={(e) => setIsMicCamToggledOn(e.target.checked)}
+          />
+          Join With mic and camera off..
+        </label>
+        <DeviceSettings />
+      </div>
+      <button
+        className="rounded-mg bg-green-500 px-4 py-2.5"
+        onClick={() => {
+          call.join();
+
+          setIsSetupComplete(true)
+        }}
+      >
+        Join Meeting
+      </button>
     </div>
   );
 };
