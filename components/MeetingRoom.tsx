ea26fca9c40e54a94f76c2a7dc9a6197af8a428c
universeBuddy <rai358788@gmail.com>
2024-08-21 19:30:42 +0530

    finding the bug

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,31 +1,29 @@
-import { PaginatedGridLayout, SpeakerLayout } from "@stream-io/video-react-sdk";
+import { cn } from "@/lib/utils";
+import {
+  CallControls,
+  CallParticipantsList,
+  PaginatedGridLayout,
+  SpeakerLayout,
+} from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
 
-type CallLayoutRoom = "grid" | "speker-left" | "speker-right";
+type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
-  const [layout, setLayout] = useState<CallLayoutType>("speker-left");
+  const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
 
-  const [showParticipants, setShowParticipants] = useState(false)
+  const [showParticipants, setShowParticipants] = useState(false);
 
+  const CallLayout = () => {
+    switch (layout) {
+      case "grid":
+        return <PaginatedGridLayout />;
 
-const CallLayout = ()=>{
-
-
-  switch(layout){
-    case 'grid':
-      return <PaginatedGridLayout />
-
-      case 'speker-right':
-        return <SpeakerLayout 
-        participantsBarPosition='left' />
+      case "speaker-right":
+        return <SpeakerLayout participantsBarPosition="left" />;
       default:
-        return <SpeakerLayout 
-        participantsBarPosition='right' />
-      
-  }
-}
-
-
+        return <SpeakerLayout participantsBarPosition="right" />;
+    }
+  };
 
   return (
     <section className="relative h-screen w-full oveflow-hidden pt-4 text-white">
@@ -33,7 +31,17 @@ const CallLayout = ()=>{
         <div className="flex size-full max-w-[1000px] items-center ">
           <CallLayout />
         </div>
-        <div className="h-[calc(100vh-80px)] hidden ml-2"></div>
+        <div
+          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
+            "show-block": showParticipants
+          })}
+        >
+          <CallParticipantsList onClose={() => setShowParticipants(false)} />
+        </div>
+      </div>
+
+      <div className="fxed bottom-0 flex w-full items-center justify-center gap-5">
+        <CallControls />
       </div>
     </section>
   );
