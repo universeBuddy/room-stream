bd6027da7d97357dc095105fe9b68d8c35d8d8fb
universeBuddy <rai358788@gmail.com>
2024-08-22 12:46:34 +0530

    adding the admin user

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -3,6 +3,7 @@ import "@stream-io/video-react-sdk/dist/css/styles.css";
 import {
   CallControls,
   CallParticipantsList,
+  CallStatsButton,
   PaginatedGridLayout,
   SpeakerLayout,
 } from "@stream-io/video-react-sdk";
@@ -15,10 +16,19 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { LayoutList } from "lucide-react";
+import { LayoutList, Users } from "lucide-react";
+import { useSearchParams } from "next/navigation";
+import EndCallButton from "./EndCallButton";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
+
+
+'personal' => !'personal' => false => !false =>true
+undefined => !undefined =>? true =>false
 const MeetingRoom = () => {
+
+const searchParams = useSearchParams()
+  const isPersonalRoom = searchParams.get('personal')
   const [layout, setLayout] = useState<CallLayoutType>("speaker-left");
 
   const [showParticipants, setShowParticipants] = useState(false);
@@ -53,17 +63,35 @@ const MeetingRoom = () => {
       <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
         <CallControls />
         <DropdownMenu>
-
           <div className="flex items-center">
-          <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]"><LayoutList size={20} className="text-white" /></DropdownMenuTrigger>
-
+            <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]">
+              <LayoutList size={20} className="text-white" />
+            </DropdownMenuTrigger>
           </div>
           <DropdownMenuContent className="border-dark-1 b-g-dark-1 tetx-white">
-        
+            {["Grid", "Speaker-Left", "Speaker-Right"].map((item, index) => (
+              <div key={index}>
+                <DropdownMenuItem
+                  className="cursor-pointer"
+                  onClick={() => {
+                    setLayout(item.toLocaleLowerCase() as CallLayoutType);
+                  }}
+                >
+                  {item}
+                </DropdownMenuItem>
+              </div>
+            ))}
             <DropdownMenuSeparator />
-        
           </DropdownMenuContent>
         </DropdownMenu>
+
+        <CallStatsButton />
+        <button onClick={() => setShowParticipants((prev) => !prev)}>
+          <div className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py- hover:bg-[#4c535b]">
+            <Users size={20} className="text-white" />
+          </div>
+        </button>
+        { !isPersonalRoom && <EndCallButton />}
       </div>
     </section>
   );
