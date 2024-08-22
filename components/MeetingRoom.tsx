b13abcf0d09764d671a0f2082380d32712eee540
universeBuddy <rai358788@gmail.com>
2024-08-22 11:19:12 +0530

    adding the menus buttons

--- components/MeetingRoom.tsx
+++ components/MeetingRoom.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import "@stream-io/video-react-sdk/dist/css/styles.css";
 import {
   CallControls,
   CallParticipantsList,
@@ -6,6 +7,15 @@ import {
   SpeakerLayout,
 } from "@stream-io/video-react-sdk";
 import React, { useState } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { LayoutList } from "lucide-react";
 
 type CallLayoutType = "grid" | "speaker-left" | "speaker-right";
 const MeetingRoom = () => {
@@ -26,22 +36,34 @@ const MeetingRoom = () => {
   };
 
   return (
-    <section className="relative h-screen w-full oveflow-hidden pt-4 text-white">
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
       <div className="relative flex size-full items-center justify-center">
         <div className="flex size-full max-w-[1000px] items-center ">
           <CallLayout />
         </div>
         <div
-          className={cn('h-[calc(100vh-86px)] hidden ml-2', {
-            "show-block": showParticipants
+          className={cn("h-[calc(100vh-86px)] hidden ml-2", {
+            "show-block": showParticipants,
           })}
         >
           <CallParticipantsList onClose={() => setShowParticipants(false)} />
         </div>
       </div>
 
-      <div className="fxed bottom-0 flex w-full items-center justify-center gap-5">
+      <div className="fixed bottom-0 flex w-full items-center justify-center gap-5">
         <CallControls />
+        <DropdownMenu>
+
+          <div className="flex items-center">
+          <DropdownMenuTrigger className="cursor-pointer rounded-2xl bg-[#19232d] px-4 py-2 hover:bg-[#4c535b]"><LayoutList size={20} className="text-white" /></DropdownMenuTrigger>
+
+          </div>
+          <DropdownMenuContent className="border-dark-1 b-g-dark-1 tetx-white">
+        
+            <DropdownMenuSeparator />
+        
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </section>
   );
